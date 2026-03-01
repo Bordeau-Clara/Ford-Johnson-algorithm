97a643a2d1856cebf943d9d37b871aa230cd4536
Clara <clara.bordeau@gmail.com>
2026-03-01 15:49:13 +0100

    sizeOfElement in includes

--- v2/includes/List.hpp
+++ v2/includes/List.hpp
@@ -6,7 +6,7 @@
 /*   By: cbordeau <bordeau@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 10:36:30 by cbordeau          #+#    #+#             */
-/*   Updated: 2026/02/14 12:09:25 by cbordeau         ###   LAUSANNE.ch       */
+/*   Updated: 2026/03/01 15:47:50 by cbordeau         ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 extern int ListComp;
 //list PMerge
 void		PMerge(std::list<int> *main);
-void		insertPend(std::list<int> *main, std::list<int> pend, int sizeOfPair);
-void		swapMain(std::list<int> *main, std::list<int>::size_type sizeOfPair);
-void		swap(std::list<int> *lst, int sizeOfPair, int idA, int idB);
+void		insertPend(std::list<int> *main, std::list<int> pend, int sizeOfElement);
+void		swapMain(std::list<int> *main, std::list<int>::size_type sizeOfElement);
+void		swap(std::list<int> *lst, int sizeOfElement, int idA, int idB);
 void		insert(std::list<int> *lst, std::list<int> toInsert, int id);
-int			holdingToConsider(std::list<int> holding, int nbToPlaceId, int sizeOfPair);
-int			placeNumber(std::list<int> *main, std::list<int> pend, int nbToPlaceId, int borne, int sizeOfPair);
-void		printList(std::list<int> list, int sizeOfPair);
+int			holdingToConsider(std::list<int> holding, int nbToPlaceId, int sizeOfElement);
+int			placeNumber(std::list<int> *main, std::list<int> pend, int nbToPlaceId, int borne, int sizeOfElement);
+void		printList(std::list<int> list, int sizeOfElement);
 void		printInfo(int nbToPlaceId, int pairId, std::list<int> pend, std::list<int> main, int decalage);
 int			getNbrFromList(std::list<int>& lst, int index);
--- v2/includes/Vector.hpp
+++ v2/includes/Vector.hpp
@@ -15,13 +15,13 @@
 
 extern int VectComp;
 //vector PMerge
-void		PMerge(std::vector<int> *main);
-void		insertPend(std::vector<int> *main, std::vector<int> pend, int sizeOfPair);
-void		swapMain(std::vector<int> *main, std::vector<int>::size_type sizeOfPair);
-void		swap(std::vector<int> *vec, int sizeOfPair, int idA, int idB);
+void		PMerge(std::vector<int> *main, int sizeOfElement);
+void		insertPend(std::vector<int> *main, std::vector<int> pend, int sizeOfElement);
+void		swapMain(std::vector<int> *main, std::vector<int>::size_type sizeOfElement);
+void		swap(std::vector<int> *vec, int sizeOfElement, int idA, int idB);
 void		insert(std::vector<int> *vec, std::vector<int> toInsert, int id);
-int			holdingToConsider(std::vector<int> holding, int nbToPlaceId, int sizeOfPair);
-int			placeNumber(std::vector<int> *main, std::vector<int> pend, int nbToPlaceId, int borne, int sizeOfPair);
-void		printList(std::vector<int> list, int sizeOfPair);
+int			holdingToConsider(std::vector<int> holding, int nbToPlaceId, int sizeOfElement);
+int			placeNumber(std::vector<int> *main, std::vector<int> pend, int nbToPlaceId, int borne, int sizeOfElement);
+void		printList(std::vector<int> list, int sizeOfElement);
 void		printInfo(int nbToPlaceId, int pairId, std::vector<int> pend, std::vector<int> main, int decalage);
 
